38fdfde0a099fd75fc46343c56061bb0acc8345b
vnema <valentina.nm@gmail.com>
2018-05-20 15:59:38 +0200

    sicrs/dialogflow-webhook-weather webhook

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ Get a WWO API key here: https://developer.worldweatheronline.com/api/
 
 import json
 import os
+import pyowm
 
 from flask import Flask, request, make_response, jsonify
 
@@ -32,6 +33,56 @@ from forecast import Forecast, validate_params
 app = Flask(__name__)
 log = app.logger
 
+owmapikey=os.environ.get('OWMApiKey') #or provide your key here
+owm = pyowm.OWM(owmapikey)
+
+#geting and sending response to dialogflow
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+    
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+#processing the request from dialogflow
+def processRequest(req):
+    
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get('address').get('city')
+    observation = owm.weather_at_place(city)
+    w = observation.get_weather()
+    latlon_res = observation.get_location()
+    lat=str(latlon_res.get_lat())
+    lon=str(latlon_res.get_lon())
+     
+    wind_res=w.get_wind()
+    wind_speed=str(wind_res.get('speed'))
+    
+    humidity=str(w.get_humidity())
+    
+    celsius_result=w.get_temperature('celsius')
+    temp_min_celsius=str(celsius_result.get('temp_min'))
+    temp_max_celsius=str(celsius_result.get('temp_max'))
+    
+    fahrenheit_result=w.get_temperature('fahrenheit')
+    temp_min_fahrenheit=str(fahrenheit_result.get('temp_min'))
+    temp_max_fahrenheit=str(fahrenheit_result.get('temp_max'))
+    speech = "Today the weather in " + city + ": \n" + "Temperature in Celsius:\nMax temp :"+temp_max_celsius+".\nMin Temp :"+temp_min_celsius+".\nTemperature in Fahrenheit:\nMax temp :"+temp_max_fahrenheit+".\nMin Temp :"+temp_min_fahrenheit+".\nHumidity :"+humidity+".\nWind Speed :"+wind_speed+"\nLatitude :"+lat+".\n  Longitude :"+lon
+    
+    return {
+        "speech": speech,
+        "displayText": speech,
+        "source": "dialogflow-weather-by-satheshrgs"
+        }
 
 @app.route('/', methods=['POST'])
 def webhook():
